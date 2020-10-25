93fb9d0f06ab0d94d605b94dbab7c7b2cacb6621
Sergei Tynianov <s.tynyanov@corp.vk.com>
2020-10-25 14:33:10 +0300

    remove duplicated code in query servlet

--- src/main/java/ru/stynyanov/refactoring/servlet/QueryServlet.java
+++ src/main/java/ru/stynyanov/refactoring/servlet/QueryServlet.java
@@ -11,91 +11,69 @@ public class QueryServlet extends CommonProductServlet {
     protected String executeRequest(HttpServletRequest request) {
         StringBuilder stringBuilder = new StringBuilder("<html><body>\n");
         String command = request.getParameter("command");
+        try {
+            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery(sqlQueryForCommand(command));
+                stringBuilder.append(htmlTitleForCommand(command));
 
-        if ("max".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                    stringBuilder.append("<h1>Product with max price: </h1>\n");
+                switch (command) {
+                    case "sum":
+                    case "count":
+                        if (rs.next()) {
+                            stringBuilder.append(rs.getInt(1)).append("\n");
+                        }
+                        break;
+                    case "max":
+                    case "min":
+                        while (rs.next()) {
+                            String name = rs.getString("name");
+                            int price = rs.getInt("price");
 
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-
-                        stringBuilder.append(name).append("\t").append(price).append("</br>\n");
-                    }
-
-                    rs.close();
-                    stmt.close();
+                            stringBuilder.append(name).append("\t").append(price).append("</br>\n");
+                        }
+                        break;
                 }
 
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                rs.close();
+                stmt.close();
             }
-        } else if ("min".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                    stringBuilder.append("<h1>Product with min price: </h1>\n");
-
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-
-                        stringBuilder.append(name).append("\t").append(price).append("</br>\n");
-                    }
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("sum".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                    stringBuilder.append("Summary price: \n");
-
-                    if (rs.next()) {
-                        stringBuilder.append(rs.getInt(1)).append("\n");
-                    }
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("count".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                    stringBuilder.append("Number of products: \n");
-
-                    if (rs.next()) {
-                        stringBuilder.append(rs.getInt(1)).append("\n");
-                    }
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            stringBuilder.append("Unknown command: ").append(command);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         stringBuilder.append("</body></html>");
         return stringBuilder.toString();
+    }
+
+    private String sqlQueryForCommand(String command) {
+        switch (command) {
+            case "sum":
+                return "Summary price: \n";
+            case "max":
+                return "<h1>Product with max price: </h1>\n";
+            case "min":
+                return "<h1>Product with min price: </h1>\n";
+            case "count":
+                return "Number of products: \n";
+            default:
+                return "Unknown command: " + command + "\n";
+        }
+    }
+
+    private String htmlTitleForCommand(String command) {
+        switch (command) {
+            case "sum":
+                return "SELECT SUM(price) FROM PRODUCT";
+            case "max":
+                return "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+            case "min":
+                return "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+            case "count":
+                return "SELECT COUNT(*) FROM PRODUCT";
+            default:
+                throw new RuntimeException("Unknown command: " + command);
+        }
     }
 
 }
